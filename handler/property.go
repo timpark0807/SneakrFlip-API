7b663ab228d4723bb39fe0f37a4f43226ca411ee
Tim Park <timpark0807@gmail.com>
2020-04-11 22:34:14 -0700

    Query all property by email

--- handler/property.go
+++ handler/property.go
@@ -3,17 +3,59 @@ package handler
 import (
 	"context"
 	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 	"github.com/timpark0807/restapi/helper"
 	"github.com/timpark0807/restapi/model"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"gopkg.in/mgo.v2/bson"
 )
 
 // ListProperties Comment
 func ListProperties(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	bearerToken, err := helper.CheckToken(r.Header.Get("Authorization"))
+
+	var results []*model.Property
+	// var params = mux.Vars(r)
+	// filter := bson.M{"createdby": params["email"]}
+
+	collection := helper.ConnectDB()
+	filter := bson.M{"createdby": bearerToken.Email}
+	findOptions := options.Find()
+	findOptions.SetLimit(10)
+
+	cur, err := collection.Find(context.TODO(), filter, findOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for cur.Next(context.TODO()) {
+
+		// create a value into which the single document can be decoded
+		var property model.Property
+		err := cur.Decode(&property)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		results = append(results, &property)
+	}
+
+	if err := cur.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Close the cursor once finished
+	cur.Close(context.TODO())
+
+	json.NewEncoder(w).Encode(results)
+
 }
 
 // CreateProperty Comment
@@ -21,11 +63,12 @@ func CreateProperty(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	bearerToken, err := helper.CheckToken(r.Header.Get("Authorization"))
-
+	fmt.Println(r.Header.Get("Authorization"))
 	var property model.Property
 	property.CreatedBy = bearerToken.Email
 
 	if err != nil {
+		fmt.Println("token invalid")
 		return
 	}
 
@@ -63,7 +106,6 @@ func GetProperty(w http.ResponseWriter, r *http.Request) {
 	err = collection.FindOne(context.TODO(), filter).Decode(&property)
 
 	if err != nil {
-		helper.GetError(err, w)
 		return
 	}
 
@@ -89,11 +131,12 @@ func UpdateProperty(w http.ResponseWriter, r *http.Request) {
 
 	update := bson.M{
 		"$set": bson.M{
-			"id":       property.ID,
-			"address":  property.Address,
-			"zipcode":  property.Zipcode,
-			"price":    property.Price,
-			"category": property.Category},
+			"_id":       property.ID,
+			"address":   property.Address,
+			"zipcode":   property.Zipcode,
+			"price":     property.Price,
+			"category":  property.Category,
+			"createdby": property.CreatedBy},
 	}
 
 	err := collection.FindOneAndUpdate(context.TODO(), filter, update).Decode(&property)
